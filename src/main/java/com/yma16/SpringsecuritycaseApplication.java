d4d663af9a90e1271b22923c8443c3e1fe1eff3d
yongma16 <17685043634@163.com>
2020-12-14 20:02:04 +0800

    初始化

---
+++ src/main/java/com/yma16/SpringsecuritycaseApplication.java
@@ -0,0 +1,13 @@
+package com.yma16;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringsecuritycaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringsecuritycaseApplication.class, args);
+    }
+
+}
